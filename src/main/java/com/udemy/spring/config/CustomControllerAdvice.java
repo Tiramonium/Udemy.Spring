8dbbf951370b7546a5a3f4c3b35bc3b5f7542a8d
Rodrigo de Souza <rodrigop2p@outlook.com>
2021-08-20 13:55:14 -0300

    Configuração da validação dos relacionamentos das entidades

---
+++ src/main/java/com/udemy/spring/config/CustomControllerAdvice.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.config;
+
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class CustomControllerAdvice {
+    @InitBinder
+    private void activateDirectFieldAccess(DataBinder dataBinder) {
+        dataBinder.initDirectFieldAccess();
+    }
+}
